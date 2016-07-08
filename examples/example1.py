15c596ea0e6ccab191ec78cec3cd0e48ee3ff5e6
law-liet <lawliet@orion.codes>
2016-07-08 18:07:41 -0400

    new style and add sort by key

--- examples/example1.py
+++ examples/example1.py
@@ -1,4 +1,4 @@
-# Simple data processing for one file
+# simple data processing for one file
 # install uniRW by running: pip install uniRW
 # cd into examples/ directory, run: python example1.py
 
@@ -28,15 +28,17 @@ import uniRW as RW
 #   Reduce: Grade - keep the maximum
 #
 def read_grade():
-    grade_dict,_ = RW.read(file_name= './data/example1.csv',
-                           mode= 'r',
-                           key_col= 0,
-                           val_cols= [1],
-                           has_header= True,
-                           ignore_chars= ['#'],
-                           split_re= ',',
-                           map_fs= {1: lambda dummy,x: float(x)},
-                           reduce_fs= {1: max})
+    grade_dict,_ = RW.read(
+        file_name= './data/example1.csv',
+        mode= 'r',
+        key_col= 0,
+        val_cols= [1],
+        has_header= True,
+        ignore_chars= ['#'],
+        split_re= ',',
+        map_fs= {1: lambda dummy,x: float(x)},
+        reduce_fs= {1: max}
+    )
 
     return grade_dict
 
@@ -60,13 +62,15 @@ def read_grade():
 def sort_grade():
     grade_dict = read_grade()
 
-    RW.write(file_name= './data/sorted_example1.csv',
-             mode= 'w',
-             key_val_dict= grade_dict,
-             split_char= ',',
-             header= ['Name','Grade'],
-             col_names= ['key', 'Grade'],
-             sort_by= 'Grade')
+    RW.write(
+        file_name= './data/sorted_example1.csv',
+        mode= 'w',
+        key_val_dict= grade_dict,
+        split_char= ',',
+        header= ['Name','Grade'],
+        col_names= ['key', 'Grade'],
+        sort_by= 'Grade'
+    )
 
 #
 # Suppose we want to create a file with each student's grade, rank and percentile.
@@ -100,14 +104,16 @@ def percentile_grade():
     def update(state, dummy):
         state['rank'] += 1
 
-    grade_dict, state = RW.read(file_name= './data/sorted_example1.csv',
-                                mode= 'r',
-                                key_col= 0,
-                                val_cols= ['rank',1],
-                                has_header= True,
-                                split_re= ',',
-                                state= {'rank': 0},
-                                update_state= update)
+    grade_dict, state = RW.read(
+        file_name= './data/sorted_example1.csv',
+        mode= 'r',
+        key_col= 0,
+        val_cols= ['rank',1],
+        has_header= True,
+        split_re= ',',
+        state= {'rank': 0},
+        update_state= update
+    )
 
     total = state['rank']
 
@@ -119,13 +125,15 @@ def percentile_grade():
 
     percentile_dict = {k: add_per_and_rev_rank(v_dict) for k, v_dict in grade_dict.items()}
 
-    RW.write(file_name= './data/percentile_example1.csv',
-             mode= 'w',
-             key_val_dict= percentile_dict,
-             split_char= ',',
-             header= ['Name', 'Rank', 'Grade', 'Percentile'],
-             col_names= ['key', 'rank', 'Grade', 'percentile'],
-             sort_by= 'percentile')
+    RW.write(
+        file_name= './data/percentile_example1.csv',
+        mode= 'w',
+        key_val_dict= percentile_dict,
+        split_char= ',',
+        header= ['Name', 'Rank', 'Grade', 'Percentile'],
+        col_names= ['key', 'rank', 'Grade', 'percentile'],
+        sort_by= 'percentile'
+    )
 
 
 
