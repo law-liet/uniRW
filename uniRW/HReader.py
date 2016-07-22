cd6cd3e18e8d85d99d9252010aadb0b492129175
law-liet <lawliet@orion.codes>
2016-07-22 00:56:38 -0400

    add HReader read,traverse,merge

--- uniRW/HReader.py
+++ uniRW/HReader.py
@@ -1,7 +1,133 @@
+from uniRW.Value import Value, StateValue
+from uniRW.State import State
+from uniRW.File import DataFile
+from copy import copy
+
 class HReader:
 
     def __init__(self, hierarchy, state=None, filter_f=lambda _,x: True):
-        pass
+        self.hierarchy = hierarchy
+        self.state = state
+        self.filter_f = filter_f
 
-    def read(self, data_file, mode='r'):
-        pass
+    def merge(self, layer, hierarchy1, hierarchy2):
+        merged_hierarchy = hierarchy1.copy()
+        inter_hierarchy = {}
+
+        if type(layer) is list:
+
+            for value in layer:
+                val1 = hierarchy1[value.name]
+                val2 = hierarchy2[value.name]
+                new_val = value.reduce_f(val1,val2)
+                inter_hierarchy[value.name] = new_val
+
+        elif type(layer) is dict:
+            value, next_layer = layer.items()[0]
+            for val, next_val_layer2 in hierarchy2.items():
+                if val in hierarchy1:
+                    next_val_layer1 = hierarchy1[val]
+                    inter_hierarchy[val] = self.merge(next_layer, next_val_layer1, next_val_layer2)
+                else:
+                    inter_hierarchy[val] = next_val_layer2
+        else:
+            raise ValueError("Wrong hierarchy structure")
+
+        merged_hierarchy.update(inter_hierarchy)
+
+        return merged_hierarchy
+
+    def traverse(self, layer, data_file, current_state, value_hierarchy):
+
+        if type(layer) is list:
+
+            for value in layer:
+
+                if isinstance(value, StateValue):
+                    val_name = value.name
+                    if current_state.check(val_name):
+                        val = value.map_f(current_state, current_state.get(val_name))
+                    else:
+                        raise KeyError(str(value.name) + " is not in state")
+                elif isinstance(value, Value):
+                    val_name = value.name
+                    val = value.map_f(current_state, data_file.line.get_by_name(val_name))
+                else:
+                    raise ValueError("Value is not a Value or StateValue object")
+
+                if val_name in value_hierarchy:
+                    current_val = value_hierarchy[val_name]
+                    reduced_val = value.reduce_f(current_val, val)
+                    value_hierarchy[val_name] = reduced_val
+                else:
+                    value_hierarchy[val_name] = val
+
+        elif type(layer) is dict:
+
+            value, next_layer = layer.items()[0]
+
+            if isinstance(value, StateValue):
+                val_name = value.name
+                if current_state.check(val_name):
+                    val = value.map_f(current_state, current_state.get(val_name))
+                else:
+                    raise KeyError(str(value.name) + " is not in state")
+            elif isinstance(value, Value):
+                val_name = value.name
+                val = value.map_f(current_state, data_file.line.get_by_name(val_name))
+            else:
+                raise ValueError("Value is not a Value or StateValue object")
+
+            if val in value_hierarchy:
+                new_hierarchy = {}
+                self.traverse(next_layer, data_file, current_state, new_hierarchy)
+                current_hierarchy = value_hierarchy[val]
+                merged_hierarchy = self.merge(next_layer, current_hierarchy, new_hierarchy)
+                value_hierarchy[val] = merged_hierarchy
+
+            else:
+                value_hierarchy[val] = {}
+                self.traverse(next_layer, data_file, current_state, value_hierarchy[val])
+
+        else:
+            raise ValueError("Wrong hierarchy structure")
+    
+    def read(self, data_file, mode='r', carry_state=False):
+
+        if not isinstance(data_file, DataFile):
+            raise ValueError("Data file is not a DataFile object.")
+
+        lineno = 0
+        value_hierarchy = {}
+        current_state = None
+        if self.state != None:
+            if not carry_state:
+                current_state = copy(self.state)
+            else:
+                current_state = self.state
+
+        with open(data_file.file_name, mode) as file:
+
+             for line in file:
+
+                data_file.line.set(line[:-1])
+
+                if data_file.header_lineno == lineno:
+                    data_file.line.set_header()
+                    lineno += 1
+                    continue
+
+                if self.state != None:
+                    current_state.release()
+                    current_state.update(data_file.line)
+                    current_state.lock()
+
+                if not self.filter_f(current_state, data_file.line):
+                    lineno += 1
+                    continue
+
+                self.traverse(self.hierarchy, data_file, current_state, value_hierarchy)
+
+                lineno += 1
+
+        return value_hierarchy, current_state
